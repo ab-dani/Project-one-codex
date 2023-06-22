0cec8ad876a5144e1efe5add6f5005302340f416
Antonio Brown <abrownjr12@outlook.com>
2023-06-22 00:26:53 -0500

    javascript

---
+++ app.js
@@ -0,0 +1,15 @@
+
+alert("Olá");
+
+document.write("Hello World!");
+
+console.log("Bonjour!");
+
+
+let age = prompt("How old are you?");
+
+if (age < 18) {
+  alert("You cannot apply");
+} else {
+  alert("You can apply");
+}
