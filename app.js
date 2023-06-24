f4ffeb204be81934d70d7074ccb2cb8d9a331a84
Antonio Brown <abrownjr12@outlook.com>
2023-06-24 03:07:00 -0500

    dsjvoidfnvdfinvfindefoivndfionvc

--- app.js
+++ app.js
@@ -13,16 +13,6 @@ document.write("Hello World!");
 
 
 
-  function name() {
-    let name = prompt("What's your name?");
-  
-    if (name === "Antonio") {
-      alert("Your name comes from the Latin -> Antonio");
-    }
-  }
-  
-  sayFact();
-
 
 if (age < 18) {
   alert("You cannot apply");
