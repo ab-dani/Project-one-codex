cf068699fb8e4cb16efa1f2ec05781265a28e420
Antonio Brown <abrownjr12@outlook.com>
2023-06-24 20:41:19 -0500

    igiluguig

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ function displayRating(){
   let output = "";
   let rating = prompt('Rate my page on a scale of 1-5');
   for (let i = 0; i < rating; i++){
-    output += "<img class = 'bigmac' src='data:https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1899&q=80'/>"
+    output += "<img class = 'phto' src='data:https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1899&q=80'/>"
   }
 document.write(output)
 }
